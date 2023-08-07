8a0ec6d71bf80b4d639d44dcee91b411ca63dd89
Muxammadaziz <muxammadazizramziddinov@gmail.com>
2023-08-07 15:50:15 +0500

    Uzbekcha

--- src/components/LanguagesTab/index.jsx
+++ src/components/LanguagesTab/index.jsx
@@ -17,7 +17,7 @@ const LanguagesTab = (props) => {
         <InputsWrapper title='Выберите язык описания' {...props}>
             <div className={cls.body}>
                 <GreyButton active={params.get('lang') === 'ru'} onClick={() => setSearchParams({ lang: 'ru' })}>Русский</GreyButton>
-                <GreyButton active={params.get('lang') === 'uz'} onClick={() => setSearchParams({ lang: 'uz' })}>Uzbekcha</GreyButton>
+                <GreyButton active={params.get('lang') === 'uz'} onClick={() => setSearchParams({ lang: 'uz' })}>O'zbekcha</GreyButton>
                 <GreyButton active={params.get('lang') === 'tr'} onClick={() => setSearchParams({ lang: 'tr' })}>Turkic</GreyButton>
                 <GreyButton active={params.get('lang') === 'en'} onClick={() => setSearchParams({ lang: 'en' })}>English</GreyButton>
                 <GreyButton active={params.get('lang') === 'tj'} onClick={() => setSearchParams({ lang: 'tj' })}>Tajik</GreyButton>
